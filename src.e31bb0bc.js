f0c743b991f6a90b4974b9db78c1a54eb0bc0165
Vivian Chen <vivianchentl@gmail.com>
2021-07-03 00:53:02 +0800

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -50835,7 +50835,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63765" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59553" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
