4b285c81f5938d168e5ab7f64710414cefe9b625
Vivian Chen <vivianchentl@gmail.com>
2021-07-03 00:16:34 +0800

    swap texture with buttons

---
+++ src/assets/index.js
@@ -0,0 +1,21 @@
+import Running from "./cat-running.png";
+import Sleeping from "./cat-sleeping.png";
+import Standing from "./cat-standing.png";
+import TiltHead from "./cat-tilt-head.png";
+import Begging from "./cat-begging.png";
+import Bathing from "./cat-bathing.png";
+
+export const Cat = {
+  running: Running,
+  sleeping: Sleeping,
+  standing: Standing,
+  tilthead: TiltHead,
+  begging: Begging,
+  bathing: Bathing,
+  // running: "https://i.imgur.com/lBvFhjW.png",
+  // sleeping: "https://i.imgur.com/jieWhho.png",
+  // standing: "https://i.imgur.com/jieWhho.png",
+  // tilthead: "https://i.imgur.com/gvyUiZs.png",
+  // begging: "https://i.imgur.com/L3eWRJF.png",
+  // bathing: "https://i.imgur.com/wXBWU1h.png",
+};
--- src/index.js
+++ src/index.js
@@ -1,52 +1,130 @@
-import * as PIXI from "pixi.js";
-import { Cat } from "./img";
+import {
+  Application,
+  AnimatedSprite,
+  Loader,
+  utils,
+  Texture,
+  Graphics,
+  Text,
+} from "pixi.js";
+import { Cat } from "./assets";
 
 let type = "WebGL";
-if (!PIXI.utils.isWebGLSupported()) {
+if (!utils.isWebGLSupported()) {
   type = "canvas";
 }
 
-PIXI.utils.sayHello(type);
+utils.sayHello(type);
 
 //Create a Pixi Application
-let app = new PIXI.Application({
-  width: 1200, // default: 800
-  height: 1200, // default: 600
+let app = new Application({
+  width: 800, // default: 800
+  height: 360, // default: 600
   antialias: true, // default: false
   backgroundAlpha: 1,
   resolution: 1, // default: 1
 });
 
+app.renderer.backgroundColor = 0xc4c4c4;
 //Add the canvas that Pixi automatically created for you to the HTML document
 document.body.appendChild(app.view);
-PIXI.Loader.shared
-  .add("cat_standing", Cat.standing)
-  .add("cat_running", Cat.running)
-  .load(onAssetsLoaded);
 
-function onAssetsLoaded() {
-  let catframes = [];
+const loader = Loader.shared;
+loader.onLoad.add((loader, resource) => {
+  //Display the file `url` currently being loaded
+  console.log("loading: " + resource.url);
 
-  // var cat_default = PIXI.Texture.from(Cat.standing);
-  // var cat_running = PIXI.Texture.from(Cat.running);
+  //Display the percentage of files currently loaded
+  console.log("progress: " + loader.progress + "%");
 
-  catframes = [PIXI.Texture.from(Cat.standing), PIXI.Texture.from(Cat.running)];
+  //If you gave your files names as the first argument
+  //of the `add` method, you can access them like this
+  //console.log("loading: " + resource.name);
+});
 
-  const anim = new PIXI.AnimatedSprite(catframes);
+loader.add("assets/nekonin.json").load(onAssetsLoaded);
 
-  anim.x = app.screen.width / 2;
-  anim.y = app.screen.height / 2;
-  anim.anchor.set(0.5);
-  anim.animationSpeed = 0.01;
-  anim.play();
+let nekonin;
 
-  app.stage.addChild(anim);
-  console.log(app.stage);
+function onAssetsLoaded(e, r) {
+  let sheet = Loader.shared.resources["assets/nekonin.json"].spritesheet;
+
+  nekonin = new AnimatedSprite(sheet.animations["nekonin-left"]);
+
+  nekonin.x = app.screen.width / 2;
+  nekonin.y = app.screen.height / 2;
+  nekonin.width = 80;
+  nekonin.height = 80;
+  nekonin.anchor.set(0.5);
+  nekonin.animationSpeed = 0.1;
+  nekonin.play();
+
+  app.stage.addChild(nekonin);
+
+  let button3 = createButton(
+    3,
+    app.screen.width - 40,
+    app.screen.height - 40,
+    "3"
+  );
+  let button2 = createButton(
+    2,
+    app.screen.width - 96,
+    app.screen.height - 40,
+    "2"
+  );
+  let button1 = createButton(
+    1,
+    app.screen.width - 152,
+    app.screen.height - 40,
+    "1"
+  );
+  app.stage.addChild(button1, button2, button3);
+
+  let direction = -1;
   // Animate the rotation
   app.ticker.add(() => {
-    anim.rotation += 0.01;
+    nekonin.x += direction;
+
+    if (nekonin.x == app.screen.width) {
+      direction = -1;
+    }
+    if (nekonin.x == 0) {
+      direction = 1;
+    }
   });
 }
 
 // const cat_bathing = PIXI.Sprite.from("./img/cat-begging.png");
 // app.stage.addChild(cat_bathing);
+function createButton(id, x, y, label) {
+  let button = new Graphics();
+  button.lineStyle(1, 0xffffff, 1);
+  button.beginFill(0xffffff, 0.25);
+  button.drawCircle(x, y, 24);
+  button.endFill();
+
+  let text = new Text(`${label}`);
+  text.anchor.set(0.5);
+  text.x = x;
+  text.y = y;
+  text.style = { fill: 0xffffff };
+
+  button.interactive = true;
+  button.buttonMode = true;
+  button.addChild(text);
+  button.on("pointerdown", () => changeState(id));
+  return button;
+}
+
+function changeState(buttonId) {
+  let sheet = Loader.shared.resources["assets/nekonin.json"].spritesheet;
+  if (buttonId == 1) {
+    nekonin.textures = sheet.animations["nekonin-left"];
+  } else if (buttonId == 2) {
+    nekonin.textures = sheet.animations["nekonin-front"];
+  } else if (buttonId == 3) {
+    nekonin.textures = sheet.animations["nekonin-right"];
+  }
+  nekonin.play();
+}
